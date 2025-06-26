99136ec62c64e14f8b44c345355fec1bf7c303c0
ChandlerWang14 <85857627+ChandlerWang14@users.noreply.github.com>
2025-06-26 09:10:33 +0000

    Deploying to gh-pages from @ ChandlerWang14/ChandlerWang14.github.io@d63afb9c834568436eb6e072d462ca9fed656215 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,8 +24,8 @@ ninja.data = [{
             },},{id: "news-two-papers-accepted-by-siggraph-2025",
           title: 'Two papers accepted by Siggraph 2025!',
           description: "",
-          section: "News",},{id: "news-one-paer-accepted-by-iccv-2025",
-          title: 'One paer accepted by ICCV 2025!',
+          section: "News",},{id: "news-one-paper-accepted-by-iccv-2025",
+          title: 'One paper accepted by ICCV 2025!',
           description: "",
           section: "News",},{
         id: 'social-email',
